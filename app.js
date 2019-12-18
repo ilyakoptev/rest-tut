1f95c84fc7a3ddd735f5c02e5bdb787d37173754
ilyakoptev <ilya.koptev@gmail.com>
2019-12-18 14:50:49 +0200

    add middleware to test

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express')
 
 const app = express()
+    // middlewares
+
+app.use('/posts', () => {
+    console.log('This is middleware running')
+})
 
 // routes
 
 app.get('/', (req, res) => {
     res.send('We are on home')
+})
+
+app.get('/posts', (req, res) => {
+    res.send('We are on posts')
 })
 
 // how to we start listening to teh server 
