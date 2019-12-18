cd2e73fe77ded81bccba77d69fcf6a1f83aec3cf
ilyakoptev <ilya.koptev@gmail.com>
2019-12-18 17:17:39 +0200

    add body-parser pack

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 require("dotenv/config");
 // middlewares
 
