d97803093442e2f2586cee82ac3c34f45deed3c8
ilyakoptev <ilya.koptev@gmail.com>
2019-12-18 19:12:54 +0200

    add cors package to access API from anywere

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 require("dotenv/config");
-app.use(bodyParser.json());
 
+app.use(bodyParser.json());
+app.use(cors());
 // middlewares
 
 //import routes
