c514bb3da12bc596200edfd7f3ed956bf7d83fc8
ilyakoptev <ilya.koptev@gmail.com>
2019-12-18 14:38:15 +0200

    add listen and first route

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const express = require('express')
 
 const app = express()
+
+// routes
+
+app.get('/', (req, res) => {
+    res.send('We are on home')
+})
+
+// how to we start listening to teh server 
+
+app.listen(3000)
